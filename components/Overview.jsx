4a6980fd9983cb96e4d97780ccb7e3c16da4f860
Basile <basile64.v@gmail.com>
2019-10-05 18:15:08 +0200

    added desc in Overview & fixing somme CSS

--- components/Overview.jsx
+++ components/Overview.jsx
@@ -5,7 +5,8 @@ import {
   faVuejs,
   faNodeJs,
   faUbuntu,
-  faBootstrap
+  faBootstrap,
+  faGitAlt
 } from "@fortawesome/free-brands-svg-icons";
 
 import Section from "./Section";
@@ -15,24 +16,45 @@ const Overview = () => {
   const [skills, setSkills] = useState([
     {
       id: 1,
-      icons: [faJsSquare],
-      title: "JavaScript",
-      desc: ""
+      icons: [faGitAlt],
+      title: "Versioning",
+      desc: "Working without Git is a no-go for me !"
     },
     {
       id: 2,
-      icons: [faReact, faVuejs, "redux"],
-      title: "Front End Development",
-      desc: ""
+      icons: [faJsSquare],
+      title: "JavaScript",
+      desc:
+        "JS is my current favorite language, I love the ecosystem & the syntax. Always bet on JavaScript !"
     },
     {
       id: 3,
+      icons: [faReact, faVuejs, "redux"],
+      title: "Front End Development",
+      desc:
+        "I start Frontend development with React, which i fall in love with..! Recently I tried VueJS, which v3 looks pretty neat, but I'm still in favor of React (Hooks are life !)"
+    },
+    {
+      id: 4,
       icons: [faNodeJs],
       title: "Back End Development",
-      desc: ""
+      desc:
+        "I mostly run with ExpressJS as framework, Sequelize as an ORM. Mocha+Chai for integrations tests. Pretty interested to grew up around GraphQL."
     },
-    { id: 4, icons: [faBootstrap], title: "", desc: "" },
-    { id: 5, icons: [faUbuntu], title: "Environment", desc: "" }
+    {
+      id: 5,
+      icons: [faBootstrap],
+      title: "Styling",
+      desc:
+        "As i'm not really good at designing, I love working with Boostrap to speed up the responsiveness. Recently I also looks up on Tailwind CSS, which looks cool !"
+    },
+    {
+      id: 5,
+      icons: [faUbuntu],
+      title: "Environment",
+      desc:
+        "I used to learn development in Ubuntu, and it still feel quite practical, easy to keep up-to-date, not too slow and UI clean."
+    }
   ]);
   return (
     <Section section="overview" title="What do I do">
