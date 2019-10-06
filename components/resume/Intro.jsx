a0822676eefc8529ed1541e2101d144ed9ec303d
Karnak19 <basile64.v@gmail.com>
2019-10-06 11:02:38 +0200

    [WIP] created intro resume components

---
+++ components/resume/Intro.jsx
@@ -0,0 +1,23 @@
+const Intro = () => {
+  return (
+    <div className="resume-intro py-3">
+      <div className="media flex-column flex-md-row align-items-center">
+        <img
+          className="resume-profile-image mb-3 mb-md-0 mr-md-5 ml-md-0 rounded mx-auto"
+          src="/static/portrait.jpg"
+        />
+        <div className="media-body text-left">
+          <p className="mb-0">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. A
+            molestias vel unde asperiores tenetur, voluptas itaque! Deserunt
+            distinctio, earum sunt doloribus itaque sint blanditiis excepturi
+            fugit, porro asperiores molestiae sapiente labore assumenda
+            molestias nam facere ratione exercitationem dignissimos! Sint, eius.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
--- components/resume/Resume.jsx
+++ components/resume/Resume.jsx
@@ -1,9 +1,13 @@
 import ResumeHeader from "./Header";
+import ResumeIntro from "./Intro";
 
 const Resume = () => {
   return (
     <article className="resume-wrapper mx-auto theme-bg-light p-5 mb-5 my-5 shadow-lg">
       <ResumeHeader />
+      <hr />
+      <ResumeIntro />
+      <hr />
     </article>
   );
 };
