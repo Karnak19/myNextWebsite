72d426dc1021118a4901cf771bb356bbdd4c6676
Karnak19 <basile64.v@gmail.com>
2019-10-12 00:19:18 +0200

    adding some projects to my Resume Main section

--- components/resume/Main.jsx
+++ components/resume/Main.jsx
@@ -2,23 +2,53 @@ import { useState } from "react";
 import { Col } from "reactstrap";
 
 import Section from "./Section";
+import Hr from "../Hr";
 
 const Main = () => {
   const [jobs, setJobs] = useState([
     {
       title: "ReactJS trainer",
-      company: "Wild Code School",
-      start: "February 2019",
-      end: undefined,
+      work: {
+        company: "Wild Code School",
+        start: "February 2019",
+        end: undefined
+      },
       ul: [],
       desc:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque accusantium quo nihil rem expedita quamtenetur asperiores incidunt ipsa perferendis, rerum ullam dolorum, molestias, illum magni ut. Molestiae, errortempora!"
     }
   ]);
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState([
+    {
+      title: "kissBot",
+      project: {
+        type: "Personal"
+      },
+      ul: ["NodeJS", "DiscordJS API", "Google Sheet API", "Heroku"],
+      desc:
+        "Creating a Discord Bot for video game Guild use. We discovered that it was painful to manage multiple tools for in game raid events (Raid planner, Discord, Google Sheet for strategies, etc.). So I end up to create this Bot to completely remove Raid Planner. We can create events via kissBot only in Discord, and fullfill the strategy sheet, then post the team in Discord. Pretty useful and permit to save us a ton of time !"
+    },
+    {
+      title: "myNextWebsite",
+      project: {
+        type: "Personal"
+      },
+      desc:
+        "Well, this is the website where you are actually reading this. I wanted to give a try to server-side rendering via NextJS, and what better for it to make a brand new Portfolio ?",
+      ul: [
+        "NodeJS",
+        "React",
+        "Bootstrap/Reactstrap",
+        "NextJS",
+        "React-Fontawesome"
+      ]
+    }
+  ]);
   return (
     <Col lg={8} xl={9} className="pr-0 pr-lg-5 resume-main">
       <Section items={jobs} title="Professional experiences" section="work" />
+      <hr />
+      <Section items={projects} title="Projects" section="work" />
     </Col>
   );
 };
--- components/resume/Section.jsx
+++ components/resume/Section.jsx
@@ -3,16 +3,26 @@ const ResumeSection = ({ section, title, items }) => {
   return (
     <section className={`${section}-section py-3`}>
       <h3 className={`text-uppercase resume-section-heading mb-4`}>{title}</h3>
-      {items.map(({ title, company, start, end, ul, desc }) => {
+      {items.map(({ title, work, project, ul, desc }) => {
         return (
           <div className="item">
             <Row className="item-heading align-items-center mb-2">
               <h4 className="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">
                 {title}
               </h4>
-              <Col className="item-meta text-muted text-left text-md-right">
-                {company} | {end ? `${start} - ${end}` : `since ${start}`}
-              </Col>
+              {work && (
+                <Col className="item-meta text-muted text-left text-md-right">
+                  {work.company} |{" "}
+                  {work.end
+                    ? `${work.start} - ${work.end}`
+                    : `since ${work.start}`}
+                </Col>
+              )}
+              {project && (
+                <Col className="item-meta text-muted text-left text-md-right">
+                  {project.type}
+                </Col>
+              )}
             </Row>
             <div className="item-content">
               <p>{desc}</p>
