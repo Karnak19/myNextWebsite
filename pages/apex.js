969818d60bdb7ca8ba83442bd0ecf8cb1a452ef1
Basile <basile64.v@gmail.com>
2019-11-30 19:53:36 +0100

    remove fetch to tracker gg api to ensure proper build on netlify

--- pages/apex.js
+++ pages/apex.js
@@ -6,18 +6,18 @@ const ApexLegends = () => {
   return <ComingSoon />;
 };
 
-ApexLegends.getInitialProps = async ({ req }) => {
-  const res = await axios({
-    url: 'https://public-api.tracker.gg/v2/apex/standard/profile/origin/BazESO',
-    method: 'GET',
-    headers: {
-      'TRN-Api-Key': '82316fdd-b5a6-483a-b4ae-d1fcaa54ca3a',
-      'Access-Control-Allow-Origin': 'https://tracker.gg'
-    }
-  });
-  const datas = res.data.data;
-  console.log(datas);
-  return { datas };
-};
+// ApexLegends.getInitialProps = async ({ req }) => {
+//   const res = await axios({
+//     url: 'https://public-api.tracker.gg/v2/apex/standard/profile/origin/BazESO',
+//     method: 'GET',
+//     headers: {
+//       'TRN-Api-Key': '82316fdd-b5a6-483a-b4ae-d1fcaa54ca3a',
+//       'Access-Control-Allow-Origin': 'https://tracker.gg'
+//     }
+//   });
+//   const datas = res.data.data;
+//   console.log(datas);
+//   return { datas };
+// };
 
 export default ApexLegends;
