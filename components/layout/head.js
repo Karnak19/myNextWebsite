ecc6823b5a35f7ea9408e6a84a57f8ceb82a2e8b
Basile <basile64.v@gmail.com>
2019-11-30 19:41:19 +0100

    changing hotjar script

--- components/layout/head.js
+++ components/layout/head.js
@@ -1,19 +1,5 @@
 import Head from 'next/head';
 
-const hotjar = (h, o, t, j, a, r) => {
-  h.hj =
-    h.hj ||
-    function() {
-      (h.hj.q = h.hj.q || []).push(arguments);
-    };
-  h._hjSettings = { hjid: 1594364, hjsv: 6 };
-  a = o.getElementsByTagName('head')[0];
-  r = o.createElement('script');
-  r.async = 1;
-  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-  a.appendChild(r);
-};
-
 const WithHead = () => {
   return (
     <Head>
@@ -24,7 +10,19 @@ const WithHead = () => {
         rel="stylesheet"
       />
       <link rel="stylesheet" href="/static/theme-1.css" />
-      {hotjar(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')}
+      {(function(h, o, t, j, a, r) {
+        h.hj =
+          h.hj ||
+          function() {
+            (h.hj.q = h.hj.q || []).push(arguments);
+          };
+        h._hjSettings = { hjid: 1594364, hjsv: 6 };
+        a = o.getElementsByTagName('head')[0];
+        r = o.createElement('script');
+        r.async = 1;
+        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+        a.appendChild(r);
+      })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')}
     </Head>
   );
 };
