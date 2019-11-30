ff1545f0cbc00f468561df6703c796ac0a568717
Basile <basile64.v@gmail.com>
2019-11-30 19:37:04 +0100

    add hotjar script

--- components/layout/head.js
+++ components/layout/head.js
@@ -1,4 +1,18 @@
-import Head from "next/head";
+import Head from 'next/head';
+
+const hotjar = (h, o, t, j, a, r) => {
+  h.hj =
+    h.hj ||
+    function() {
+      (h.hj.q = h.hj.q || []).push(arguments);
+    };
+  h._hjSettings = { hjid: 1594364, hjsv: 6 };
+  a = o.getElementsByTagName('head')[0];
+  r = o.createElement('script');
+  r.async = 1;
+  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+  a.appendChild(r);
+};
 
 const WithHead = () => {
   return (
@@ -10,6 +24,7 @@ const WithHead = () => {
         rel="stylesheet"
       />
       <link rel="stylesheet" href="/static/theme-1.css" />
+      {hotjar(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')}
     </Head>
   );
 };
