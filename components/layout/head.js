01342a8013d68bbafb4ae8512548cb173371ea97
Basile <basile64.v@gmail.com>
2019-11-30 19:47:43 +0100

    add dangerously inner html, hope it works this time

--- components/layout/head.js
+++ components/layout/head.js
@@ -10,7 +10,10 @@ const WithHead = () => {
         rel="stylesheet"
       />
       <link rel="stylesheet" href="/static/theme-1.css" />
-      {(function(h, o, t, j, a, r) {
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(h, o, t, j, a, r) {
         h.hj =
           h.hj ||
           function() {
@@ -22,7 +25,9 @@ const WithHead = () => {
         r.async = 1;
         r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
         a.appendChild(r);
-      })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')}
+      })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')`
+        }}
+      />
     </Head>
   );
 };
